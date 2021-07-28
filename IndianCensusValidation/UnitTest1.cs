259d500e49cb17d9fe8e9221dcec6ede14ebe44d
Radhika <radhika.shankar1220@gmail.com>
2021-07-28 15:56:31 +0530

    [Radhika] Indian state code data

--- IndianCensusValidation/UnitTest1.cs
+++ IndianCensusValidation/UnitTest1.cs
@@ -13,6 +13,11 @@ namespace IndianCensusValidation
         string wrongstateCensusDataCSVPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCensusData.txt";
         string wrongDelimiterstateCensusDataCSVPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\WrongDelimiterIndiaStateCensusData.csv";
         string wrongHeaderstateCensusDataCSVPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\WrongHeaderIndiaStateCensusData.csv";
+        string stateCodeDataPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateCode.csv";
+        string wrongStateCodeDataPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\IndiaStateC.csv";
+        string wrongStateCodeDataCSVPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\WrongStateCodeDataCSVPath.txt";
+        string wrongDelimiterStateCodeDataPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\WrongDelimiterStateCodeDataPath.csv";
+        string wrongHeaderStateCodeDataPath = @"C:\Users\Radhika\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\WrongHeaderStateCodeDataPath.csv";
         CsvAdapterFactory csv = null;
         Dictionary<string, FullCensusData> totalRecords;
         Dictionary<string, FullCensusData> stateRecords;
@@ -71,6 +76,52 @@ namespace IndianCensusValidation
         public void Incorrect_Headers_In_CSVFile_Return_CustomException()
         {
             var customEx = Assert.ThrowsException<CensusAnalyserException>(() => csv.LoadCsvData(CensusAnalyser.Country.INDIA, wrongHeaderstateCensusDataCSVPath, "State,Population,AreaInSqKm,Density"));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_HEADERS, customEx.exception);
+        }
+        /// <summary>
+        /// UC2-TC1.1--->Returns the count of state code data in csv file
+        /// </summary>
+        [TestMethod]
+        public void State_Code_Return_CountOf_Records()
+        {
+            var totalRecords=csv.LoadCsvData(CensusAnalyser.Country.INDIA, stateCodeDataPath, "SerailNo,StateName,StateCode");
+            Assert.AreEqual(16, totalRecords.Count);
+        }
+        /// <summary>
+        /// UC2-TC1.2--->Handle the file not found custom exception
+        /// </summary>
+        [TestMethod]
+        public void Incorrect_File_Return_CustomException_State_Code()
+        {
+            var customEx = Assert.ThrowsException<CensusAnalyserException>(() => csv.LoadCsvData(CensusAnalyser.Country.INDIA, wrongStateCodeDataPath, "SerailNo,StateName,StateCode"));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, customEx.exception);
+        }
+        /// <summary>
+        /// UC2-TC1.3--->Handle the wrong file extension path using custom exception
+        /// </summary>
+        [TestMethod]
+        public void Incorrect_File_Extension_Return_CustomException_For_StateCode()
+        {
+            var customEx = Assert.ThrowsException<CensusAnalyserException>(() => csv.LoadCsvData(CensusAnalyser.Country.INDIA, wrongStateCodeDataCSVPath, "SerailNo,StateName,StateCode"));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_EXTENSION, customEx.exception);
+        }
+        /// <summary>
+        /// UC2-TC1.4--->Wrong delimiter in csvfile and handling the custom exception
+        /// </summary>
+        [TestMethod]
+        public void Incorrect_Delimiter_In_CSVFile_Return_CustomException_For_StateCode()
+        {
+            var customEx = Assert.ThrowsException<CensusAnalyserException>(() => csv.LoadCsvData(CensusAnalyser.Country.INDIA, wrongDelimiterStateCodeDataPath, "SerailNo,StateName,StateCode"));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_DELIMITER, customEx.exception);
+        }
+        /// <summary>
+        /// UC2-TC1.5--->Wrong headers in csvfile and handling the custom exception
+        /// </summary>
+        [TestMethod]
+
+        public void Incorrect_Headers_In_CSVFile_Return_CustomException_For_StateCode()
+        {
+            var customEx = Assert.ThrowsException<CensusAnalyserException>(() => csv.LoadCsvData(CensusAnalyser.Country.INDIA, wrongHeaderStateCodeDataPath,"State,Population,AreaInSqKm,Density"));
             Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_HEADERS, customEx.exception);
         }
     }
